5b78acf630e42c6741268ea24d755114baf3e4c7
Allen Kim <allenhwkim@gmail.com>
2015-08-18 23:49:14 -0400

    fixed bug of navigation font error and added test_conf.json

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var angularJsdoc = require('./index.js');
 
 angularJsdoc('sample-codes', {
+  configure: 'common/test_conf.json',
   template: 'default',
   destination: 'default/docs',
   readme: "sample-codes/README.md"
