5c0209b760d3af2edac8e68f27b9100bed44351a
Igor Defaye <Igor.Defaye@planonsoftware.com>
2015-10-16 13:34:29 -0400

    added more tags support needed for angular documentation

--- common/plugins/ngdoc.js
+++ common/plugins/ngdoc.js
@@ -21,4 +21,46 @@ exports.defineTags = function(dictionary) {
     }
   })
   .synonym('attr');
+
+  dictionary.defineTag('restrict', {
+    mustHaveValue: true,
+    onTagged: function(doclet, tag) {
+      var restricts={
+          'A': 'Attribute',
+          'E': 'Element',
+          'C': 'Class'
+      }
+      var s = tag.value.split('').map(function(aec) {
+        return restricts[aec];
+      })
+      doclet.restrict = s;
+    }
+  });
+
+  dictionary.defineTag('priority', {
+    mustHaveValue: true,
+    onTagged: function(doclet, tag) {
+      doclet.priority = tag.value;
+    }
+  });
+
+  dictionary.defineTag('eventType', {
+    mustHaveValue: true,
+    onTagged: function(doclet, tag) {
+      doclet.eventType = tag.value;
+    }
+  });
+
+  dictionary.defineTag('animations', {
+    mustHaveValue: true,
+    onTagged: function(doclet, tag) {
+      doclet.animations = tag.value;
+    }
+  });
+
+  dictionary.defineTag('scope', {
+    onTagged: function(doclet, tag) {
+      doclet.newScope = true;
+    }
+  });
 };
